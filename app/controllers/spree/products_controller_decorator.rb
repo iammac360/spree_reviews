9389e2fb149f6bac97e9ff9d189ff9d62d00857a
Mark Sargento <sarge@bazinga.com.ph>
2013-04-30 11:31:25 +0800

    Overrided the show action of products controller.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,3 +1,29 @@
 Spree::ProductsController.class_eval do
   helper Spree::ReviewsHelper
+
+  def show
+    return unless @product
+
+    @variants = @product.variants_including_master.active(current_currency).includes([:option_values, :images])
+    @product_properties = @product.product_properties.includes(:property)
+
+    @review = Spree::Review.new(:product => @product)
+    authorize! :create, @review
+
+    referer = request.env['HTTP_REFERER']
+    if referer
+      begin
+        referer_path = URI.parse(request.env['HTTP_REFERER']).path
+          # Fix for #2249
+      rescue URI::InvalidURIError
+        # Do nothing
+      else
+        if referer_path && referer_path.match(/\/t\/(.*)/)
+          @taxon = Taxon.find_by_permalink($1)
+        end
+      end
+    end
+
+    respond_with(@product)
+  end
 end
