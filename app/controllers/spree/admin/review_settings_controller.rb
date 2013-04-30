061c01043e906f8fb6fa2a78684db0fe1ca07dd9
Mark Sargento <sarge@bazinga.com.ph>
2013-04-30 11:31:02 +0800

    Added admin settings for quality, appearance and price.

--- app/controllers/spree/admin/review_settings_controller.rb
+++ app/controllers/spree/admin/review_settings_controller.rb
@@ -3,6 +3,9 @@ class Spree::Admin::ReviewSettingsController < Spree::Admin::BaseController
     # workaround for unset checkbox behaviour
     params[:preferences][:include_unapproved_reviews] = false if params[:preferences][:include_unapproved_reviews].blank?
     params[:preferences][:feedback_rating] = false            if params[:preferences][:feedback_rating].blank?
+    params[:preferences][:feedback_quality_rating] = false            if params[:preferences][:feedback_quality_rating].blank?
+    params[:preferences][:feedback_appearance_rating] = false            if params[:preferences][:feedback_appearance_rating].blank?
+    params[:preferences][:feedback_price_rating] = false            if params[:preferences][:feedback_price_rating].blank?
     params[:preferences][:show_email] = false                 if params[:preferences][:show_email].blank?
     params[:preferences][:require_login] = false              if params[:preferences][:require_login].blank?
     params[:preferences][:track_locale] = false               if params[:preferences][:track_locale].blank?
