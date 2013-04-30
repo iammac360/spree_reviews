aeab6e332bf00fd184c8d7ebe55c96ed1a5487d6
Mark Sargento <sarge@bazinga.com.ph>
2013-04-30 11:34:20 +0800

    Modified models for review spec.

--- spec/factories/review_factory.rb
+++ spec/factories/review_factory.rb
@@ -4,6 +4,9 @@ FactoryGirl.define do
     title { Faker::Lorem.words(4).join(' ') }
     review { Faker::Lorem.paragraphs(3).join("\n") }
     rating { (rand * 4).to_i + 1 }
+    quality_rating { (rand * 4).to_i + 1 }
+    appearance_rating { (rand * 4).to_i + 1 }
+    price_rating { (rand * 4).to_i + 1 }
     approved false
 
     association(:user, :factory => :user)
--- spec/models/reviews_spec.rb
+++ spec/models/reviews_spec.rb
@@ -5,6 +5,12 @@ describe Spree::Review do
     @review = FactoryGirl.create :review
   end
 
+  context "relationships" do
+    it { should belong_to(:product) }
+    it { should belong_to(:user) }
+    it { should have_many(:feedback_reviews) }
+  end
+
   context "creating a new review" do
     it "is valid with valid attributes" do
       @review.should be_valid
@@ -15,8 +21,26 @@ describe Spree::Review do
       @review.should_not be_valid
     end
 
+    it "is not valid without a quality rating" do
+      @review.quality_rating = nil
+      @review.should_not be_valid
+    end
+
+    it "is not valid without a appearance rating" do
+      @review.appearance_rating = nil
+      @review.should_not be_valid
+    end
+
+    it "is not valid without a price rating" do
+      @review.price_rating = nil
+      @review.should_not be_valid
+    end
+
     it "is not valid unless the rating is an integer" do
       @review.rating = 2.0
+      @review.quality_rating = 2.0
+      @review.appearance_rating = 2.0
+      @review.price_rating = 2.0
       @review.should_not be_valid
     end
 
@@ -45,25 +69,55 @@ describe Spree::Review do
       @review.approved = true
       @review.save!
       @review.product.reload.avg_rating.should == @review.rating
+      @review.product.reload.avg_rating_quality.should == @review.quality_rating
+      @review.product.reload.avg_rating_appearance.should == @review.appearance_rating
+      @review.product.reload.avg_rating_price.should == @review.price_rating
 
       @review.destroy
       product.reload.avg_rating.should == 0
+      product.reload.avg_rating_quality.should == 0
+      product.reload.avg_rating_appearance.should == 0
+      product.reload.avg_rating_price.should == 0
     end
   end
 
-  context "#feedback_stars" do
+  context "feedback_stars" do
     before(:each) do
       @review.save
       3.times do |i|
         f = Spree::FeedbackReview.new
         f.review = @review
         f.rating = (i+1)
+        f.quality_rating = (i+1)
+        f.appearance_rating = (i+1)
+        f.price_rating = (i+1)
         f.save
       end
     end
 
-    it "should return the average rating from feedback reviews" do
-      @review.feedback_stars.should == 2
+    context "#feedback_stars" do
+      it "should return the average rating from feedback reviews" do
+        @review.feedback_stars.should == 2
+      end
+    end
+
+    context "#feedback_quality_stars" do
+      it "should return the average quality rating from feedback reviews" do
+        @review.feedback_quality_stars.should == 2
+      end
+    end
+
+    context "#feedback_appearance_stars" do
+      it "should return the average appearance rating from feedback reviews" do
+        @review.feedback_appearance_stars.should == 2
+      end
+    end
+
+    context "#feedback_price_stars" do
+      it "should return the average price rating from feedback reviews" do
+        @review.feedback_price_stars.should == 2
+      end
     end
   end
+
 end
